7f77551f557fc8f6d92d6ac8201d07860a4afe7e
Danny Bui <Dannybui5155@gmail.com>
2023-08-19 13:34:20 -0500

    created superuser and modified admin portal

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Form
 
 # Register your models here.
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email")
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("date", "occupation")
+    # add - in front of name to reverse order
+    ordering = ("first_name", )
+    # Set to read only
+    readonly_fields = ("occupation", )
+
+admin.site.register(Form, FormAdmin)
